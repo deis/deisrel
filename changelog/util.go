b1f026e020e20ad16083429446c1627ee54fd8f7
Aaron Schlesinger <aschlesinger@deis.com>
2016-05-12 10:52:03 -0700

    fix(changelog/util.go): add asterisk for commits that have no focus

--- changelog/util.go
+++ changelog/util.go
@@ -9,7 +9,7 @@ func commitFocus(str string) string {
 	// parse only the title, strip the commit body
 	str = strings.Split(str, "\n")[0]
 	if !strings.Contains(str, "(") || !strings.Contains(str, ")") {
-		return ""
+		return "*" // return a asterisk so that commits with no focus are marked with a focus of "all"
 	}
 	// fetch the string between the parentheses
 	return strings.TrimSpace(strings.Split(strings.Split(str, ")")[0], "(")[1])
